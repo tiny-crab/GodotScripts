781f24666c219e7d17e4190111e3a8085bd29017
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2024-06-24 06:49:30 -0700

    Add tween utils doc

--- TweenUtils.gd
+++ TweenUtils.gd
@@ -1,4 +1,6 @@
 extends Node
+## Use by adding to Autoload scripts
+## I like naming it as `t` so that the final function call is `t.ween().tween_property(...)`
 
 func ween():
     return get_tree().create_tween()
